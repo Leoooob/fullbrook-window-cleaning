4f96e8d634312bd05a723cf88942366c2a7b7bcf
Leon (windows) <leonhassan@hotmail.com>
2025-02-09 15:17:27 +0000

    netlify tag converted to data-netlify

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -43,8 +43,8 @@ export default function FormBlock(props) {
             id={elementId}
             onSubmit={handleSubmit}
             ref={formRef}
-            data-sb-field-path= {fieldPath}
-            netlify
+            data-sb-field-path={fieldPath}
+            data-netlify
         >
             <div
                 className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
